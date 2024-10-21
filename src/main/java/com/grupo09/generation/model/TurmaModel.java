9fc2a1b4a8a1a25b36126253136469e927784281
milhoratti <victor.mihoratti@gmail.com>
2024-10-21 14:35:11 -0300

    feat: criação das models

--- src/main/java/com/grupo09/generation/model/TurmaModel.java
+++ src/main/java/com/grupo09/generation/model/TurmaModel.java
@@ -1,4 +1,29 @@
 package com.grupo09.generation.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class TurmaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, message = "Nome deve ter pelo menos 3 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Instrutor é obrigatório")
+    @Size(min = 3, message = "Instrutor deve ter pelo menos 3 caracteres")
+    private String instrutor;
+
+    @OneToMany(mappedBy = "turma")
+    private List<AlunoModel> alunos;
 }
