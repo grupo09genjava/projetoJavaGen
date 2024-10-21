a50c29efde12cb8988c71cc6320a5f5a83af4bc1
dmtz1989 <demetryus.oliveira@gmail.com>
2024-10-21 09:59:44 -0300

    First Commit

---
+++ src/test/java/com/grupo09/generation/GenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo09.generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
