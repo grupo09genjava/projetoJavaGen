21758463ddfb115a83c8b28369e7c145ad7c54a2
milhoratti <victor.mihoratti@gmail.com>
2024-10-21 15:07:36 -0300

    feat: repositórios

--- src/main/java/com/grupo09/generation/repository/AlunoRepository.java
+++ src/main/java/com/grupo09/generation/repository/AlunoRepository.java
@@ -1,4 +1,13 @@
 package com.grupo09.generation.repository;
 
-public interface AlunoRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.grupo09.generation.model.AlunoModel;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
+    Optional<AlunoModel> findByEmail(String email);
 }
--- src/main/java/com/grupo09/generation/repository/FuncionarioRepository.java
+++ src/main/java/com/grupo09/generation/repository/FuncionarioRepository.java
@@ -1,4 +1,13 @@
 package com.grupo09.generation.repository;
 
-public interface FuncionarioRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.grupo09.generation.model.FuncionarioModel;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
+    Optional<FuncionarioModel> findByEmail(String email);
 }
