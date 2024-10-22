374165d34876a4354976e04298d768cf45c511fa
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-10-21 23:48:15 -0300

    feat: exception personalizada para modelo de turma

---
+++ src/main/java/com/grupo09/generation/exception/TurmaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.grupo09.generation.exception;
+
+public class TurmaNotFoundException extends RuntimeException{
+    public TurmaNotFoundException(String message){
+        super(message);
+    }
+}
