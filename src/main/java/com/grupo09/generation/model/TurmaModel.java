ed86e0a6be031e92b489ecc2a0b1a015c04e28ad
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-10-21 23:45:25 -0300

    feat: adicao do metdodo addAluno e toEntity

--- src/main/java/com/grupo09/generation/model/TurmaModel.java
+++ src/main/java/com/grupo09/generation/model/TurmaModel.java
@@ -2,22 +2,19 @@ package com.grupo09.generation.model;
 
 import java.util.List;
 
+import com.grupo09.generation.dto.in.CreateTurma;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
-
 @Entity
 @Table(name = "tab_turmas")
+@Builder
 public class TurmaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +28,21 @@ public class TurmaModel {
     @Size(min = 3, message = "Instrutor deve ter pelo menos 3 caracteres")
     private String instrutor;
 
-    @OneToMany(mappedBy = "turmas")
+    @OneToMany(mappedBy = "turma")
     private List<AlunoModel> alunos;
+
+    public void addAluno(AlunoModel aluno) {
+        alunos.add(aluno);
+        aluno.setTurma(this);
+    }
+
+    public TurmaModel(String nome,String instrutor,List<AlunoModel> alunos){
+        this.nome = nome;
+        this.instrutor = instrutor;
+        this.alunos = alunos;
+    }
+
+    public static TurmaModel toEntity(String nome,String instrutor,List<AlunoModel> alunos) {
+        return new TurmaModel(nome, instrutor, alunos);
+    }
 }
