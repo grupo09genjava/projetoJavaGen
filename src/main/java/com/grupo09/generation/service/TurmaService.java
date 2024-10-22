31faa6042fe3d812f2870b4689fa6af450362e6f
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-10-22 11:53:29 -0300

    fix: correção da persistencia de alunos com a turma

--- src/main/java/com/grupo09/generation/service/TurmaService.java
+++ src/main/java/com/grupo09/generation/service/TurmaService.java
@@ -4,32 +4,37 @@ import com.grupo09.generation.dto.in.CreateAluno;
 import com.grupo09.generation.dto.in.CreateTurma;
 import com.grupo09.generation.dto.in.UpdateTurma;
 import com.grupo09.generation.dto.out.TurmaOutput;
+import com.grupo09.generation.exception.AlunoAlreadyExistException;
 import com.grupo09.generation.exception.TurmaNotFoundException;
 import com.grupo09.generation.model.AlunoModel;
 import com.grupo09.generation.model.TurmaModel;
+import com.grupo09.generation.repository.AlunoRepository;
 import com.grupo09.generation.repository.TurmaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class TurmaService {
-    private final TurmaRepository repository;
+    private final TurmaRepository turmaRepository;
+    private final AlunoRepository alunoRepository;
 
-    public TurmaService(TurmaRepository repository) {
-        this.repository = repository;
+    public TurmaService(TurmaRepository turmaRepository,AlunoRepository alunoRepository){
+        this.turmaRepository = turmaRepository;
+        this.alunoRepository = alunoRepository;
     }
 
     public List<TurmaOutput> findAll(){
-        return repository.findAll().stream()
+        return turmaRepository.findAll().stream()
                .map(TurmaOutput::fromEntity)
                .collect(Collectors.toList());
     }
 
-
-
+    @Transactional
     public TurmaOutput save(CreateTurma createTurma){
         TurmaModel turmaModel = TurmaModel.builder()
                 .nome(createTurma.nome())
@@ -39,28 +44,33 @@ public class TurmaService {
         if (createTurma.alunos() != null) {
             for (CreateAluno createAluno : createTurma.alunos()) {
                 AlunoModel aluno = AlunoModel.toEntity(createAluno);
-                turmaModel.addAluno(aluno);
+                Optional<AlunoModel> foundAluno = alunoRepository.findByEmail(aluno.getEmail());
+                if (foundAluno.isPresent()) {
+                    throw new AlunoAlreadyExistException("Email já existente no banco de dados");
+                }
+                AlunoModel savedAluno = alunoRepository.save(aluno);
+                turmaModel.addAluno(savedAluno);
             }
         }
-        repository.save(turmaModel);
+        turmaRepository.save(turmaModel);
         return TurmaOutput.fromEntity(turmaModel);
     }
 
     public TurmaOutput findById(Long id){
-        TurmaModel turmaModel = repository.findById(id).orElseThrow(() -> new TurmaNotFoundException("Turma não encontrada"));
+        TurmaModel turmaModel = turmaRepository.findById(id).orElseThrow(() -> new TurmaNotFoundException("Turma não encontrada"));
         return TurmaOutput.fromEntity(turmaModel);
     }
 
     public void deleteById(Long id){
-        TurmaModel turmaModel = repository.findById(id).orElseThrow(() -> new TurmaNotFoundException("Turma não encontrada"));
-        repository.deleteById(id);
+        TurmaModel turmaModel = turmaRepository.findById(id).orElseThrow(() -> new TurmaNotFoundException("Turma não encontrada"));
+        turmaRepository.deleteById(id);
     }
 
     public TurmaOutput put(Long id, UpdateTurma updateTurma){
-        TurmaModel turmaModel = repository.findById(id).orElseThrow(() -> new TurmaNotFoundException("Turma não encontrada"));
+        TurmaModel turmaModel = turmaRepository.findById(id).orElseThrow(() -> new TurmaNotFoundException("Turma não encontrada"));
         turmaModel.setNome(updateTurma.nome());
         turmaModel.setInstrutor(updateTurma.instrutor());
-        repository.save(turmaModel);
+        turmaRepository.save(turmaModel);
         return TurmaOutput.fromEntity(turmaModel);
     }
 }
