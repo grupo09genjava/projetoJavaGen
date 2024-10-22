5dd0b936aae3d710fd7a42e2d79a4d5ace0e0fe4
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-10-22 19:08:53 -0300

    feat: adição e alterações de validaçãos nos dtos

--- src/main/java/com/grupo09/generation/dto/out/AlunoOutput.java
+++ src/main/java/com/grupo09/generation/dto/out/AlunoOutput.java
@@ -2,7 +2,9 @@ package com.grupo09.generation.dto.out;
 
 import com.grupo09.generation.model.AlunoModel;
 
-public record AlunoOutput(Long alunoId, String name, String email, int idade, double notaPrimeiroModulo, double notaSegundoModulo, double media){
+import java.util.Optional;
+
+public record AlunoOutput(Long alunoId,String nome,String email,int idade,Optional<Double> notaPrimeiroModulo,Optional<Double> notaSegundoModulo,Optional<Double> media){
     public static AlunoOutput fromEntity(AlunoModel alunoModel){
         return new AlunoOutput(alunoModel.getId(), alunoModel.getNome(), alunoModel.getEmail(), alunoModel.getIdade(),
                             alunoModel.getNotaPrimeiroModulo(), alunoModel.getNotaSegundoModulo(),
