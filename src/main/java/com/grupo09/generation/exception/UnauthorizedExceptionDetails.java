f390f0930f8f781f1bc060469870cfb2d46d66cc
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-10-24 01:18:32 -0300

    feat: configuração do spring security e implementação do JwT

---
+++ src/main/java/com/grupo09/generation/exception/UnauthorizedExceptionDetails.java
@@ -0,0 +1,13 @@
+package com.grupo09.generation.exception;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@SuperBuilder
+public class UnauthorizedExceptionDetails extends ExceptionDetails{
+}
+
